9f4a6c314bfed8c885e5c0b520b1da7e56125043
ath4trva <atharvapatil8021@gmail.com>
2026-01-09 01:03:29 +0530

    Add godotenv dependency and initialize in main
    Added github.com/joho/godotenv to manage environment variables. Updated main.go to load .env file at startup and handle errors if the file is missing.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ticket-system
 
 go 1.25.5
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import(
+	"log"
+	"github.com/joho/godotenv"
+
+)
+
+func main(){
+	err:= godotenv.Load()
+	if err!=nil{
+		log.Fatalf("Error loading .env file")
+	}
+
+	connStr:=
+}
