d11e4ca2d779c1468d34f167bd5e09f810d231cc
ath4trva <atharvapatil8021@gmail.com>
2026-01-12 23:27:55 +0530

    Connected to database

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
-import(
+import (
+	"database/sql"
+	"fmt"
 	"log"
-	"github.com/joho/godotenv"
+	"os"
 
+	"github.com/joho/godotenv"
 )
 
-func main(){
-	err:= godotenv.Load()
-	if err!=nil{
+var db *sql.DB
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
 
-	connStr:=
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"))
+
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err:= db.Ping(); err != nil {
+		log.Fatalf("Connection failed: %v",err)
+	}
+	fmt.Println("Connection Successfull")
+
+	ctx:= context.Background()
 }
