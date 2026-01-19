91da98527a2175a997832b1007fdc94d454265b8
ath4trva <atharvapatil8021@gmail.com>
2026-01-19 20:21:51 +0530

    simulating 100 workers to book ticket

--- main.go
+++ main.go
@@ -3,149 +3,103 @@ package main
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
-	"log"
+	"log/slog"
 	"os"
+	"os/user"
+	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	"golang.org/x/tools/go/analysis/passes/defers"
+	// Postgres driver
 )
 
 var db *sql.DB
+var (
+	bookingsSuccess int64
+	bookingsFailed  int64
+	retryCount     int64
+)
 
 func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.Default(logger)
+
 	if err := godotenv.Load(); err != nil {
-		log.Println("Note: .env file not found, using system env")
+		slog.Warn("Note: .env file not found.")
 	}
 
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
 	var err error
-	db, err = sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal("Error opening database: ", err)
+	db, err= sql.Open("postgress",connStr)
+	if err!=nil{
+		slog.Error("Error opening database", "error", err)
+		os.Exit(1)
 	}
 	defer db.Close()
 
-	if err = db.Ping(); err != nil {
-		log.Fatal("Database connection failed: ", err)
-	}
-	fmt.Println("--- Successfully connected to PostgreSQL ---")
-
-	ctx := context.Background()
-
-	db.Exec("DELETE FROM bookings")
-	db.Exec("DELETE FROM events")
-
-	eventID, err := CreateEvent(ctx, "Go Conference 2026", 5)
-	if err != nil {
-		log.Fatal("Failed to create event: ", err)
-	}
-	fmt.Printf("Step 1: Event Created with ID %d\n", eventID)
-
-	fmt.Println("Step 2: Attempting to book ticket for User 101...")
-	if err := BookTicket(ctx, eventID, 101); err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Success: User 101 booked a ticket!")
+	resetDatabase()
+	totalTickets:=10
+	eventID, err:= CreateEvent(context.Background(),"Pepsi Super Bowl Halftime",totalTickets)
+	if err!=nil{
+		slog.Error("Failed to create event", "error", err)
 	}
 
-	fmt.Println("Step 3: Attempting duplicate booking for User 101...")
-	if err := BookTicket(ctx, eventID, 101); err != nil {
-		fmt.Printf("Caught Expected Error: %v\n", err)
-	}
+	slog.Info("STARTING STRESS TEST", "TICKETS", totalTickets, "USER", 100)
 
-	fmt.Println("Step 4: Attempting to cancel User 101's booking...")
-	if err := CancelBooking(ctx, eventID, 101); err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Success: Booking cancelled, ticket returned to pool.")
-	}
+	start:= time.Now()
+	runStressTest(eventID,100)
+	duration:= time.Since(start)
+
+	verifyResults(eventID, totalTickets)
+	fmt.Println("\n---------------------------------------------------")
+	fmt.Printf(" Execution Time: %v\n", duration)
+	fmt.Printf(" Successful Bookings: %d\n", bookingsSuccess)
+	fmt.Printf(" Failed Bookings:     %d\n", bookingsFailed)
+	fmt.Printf(" Retries (Conflicts): %d\n", retryCount)
+	fmt.Println("---------------------------------------------------")
+
 }
 
-func CreateEvent(ctx context.Context, name string, tickets int) (int, error) {
-	var id int
-	query := `INSERT INTO events (name, total_tickets, available_tickets) 
-              VALUES ($1, $2, $3) RETURNING id`
+func runStressTest(eventID int, concurrentUser int){
+	var wg sync.WaitGroup
 
-	err := db.QueryRowContext(ctx, query, name, tickets, tickets).Scan(&id)
-	return id, err
-}
+	for i:=0; i<=concurrentUser;i++{
+		wg.Add(1)
+		userID:=1000+i
 
-func BookTicket(ctx context.Context, eventID int, userID int) error {
-	tx, err := db.BeginTx(ctx, &sql.TxOptions{
-		Isolation: sql.LevelSerializable,
-	})
-	if err != nil {
-		return err
+	go func(uID int){
+		defer wg.Done()
+		maxRetries:=5
+		for attempt:=0; attempt<=maxRetries;attempt++{
+			ctx, cancel:= context.WithTimeout(context.Background(),2*time.Second)
+			err:= BookingTickets(ctx, eventID,uID)
+			cancel()
+
+			if err ==nil{
+				atomic.AddInt64(&bookingsSuccess,1)
+				slog.Info("Got Ticket!", "user_id", uID)
+				return
+			}
+
+			if isSerializationFailure(err){
+				atomic.AddInt64(&retryCount,1)
+				time.Sleep(time.Duration(rand.Intn(50)+10) * time.Millisecond)
+				continue
+			}
+
+			atomic.AddInt64(&bookingsFailed,1)
+			slog.Info("SOLD OUT", "user_id", uID)
+			return
+		}
+		
+		slog.Error("GAVE UP (Too many retries)", "user_id", uID)
+			atomic.AddInt64(&bookingsFailed, 1)
+		}(userID)
 	}
-	defer tx.Rollback()
-
-	var exists bool
-	checkDupQuery := `SELECT EXISTS(SELECT 1 FROM bookings WHERE event_id=$1 AND user_id=$2)`
-	err = tx.QueryRowContext(ctx, checkDupQuery, eventID, userID).Scan(&exists)
-	if err != nil {
-		return err
-	}
-	if exists {
-		return errors.New("user already has a booking for this event")
-	}
-
-	var available int
-	checkTicketsQuery := `SELECT available_tickets FROM events WHERE id=$1`
-	err = tx.QueryRowContext(ctx, checkTicketsQuery, eventID).Scan(&available)
-	if err != nil {
-		return err
-	}
-	if available <= 0 {
-		return errors.New("insufficient tickets available")
-	}
-
-	updateQuery := `UPDATE events SET available_tickets = available_tickets - 1 WHERE id=$1`
-	_, err = tx.ExecContext(ctx, updateQuery, eventID)
-	if err != nil {
-		return err
-	}
-
-	insertQuery := `INSERT INTO bookings (event_id, user_id) VALUES ($1, $2)`
-	_, err = tx.ExecContext(ctx, insertQuery, eventID, userID)
-	if err != nil {
-		return err
-	}
-
-	return tx.Commit()
-}
-
-func CancelBooking(ctx context.Context, eventID int, userID int) error {
-	tx, err := db.BeginTx(ctx, &sql.TxOptions{
-		Isolation: sql.LevelSerializable,
-	})
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	res, err := tx.ExecContext(ctx, "DELETE FROM bookings WHERE event_id=$1 AND user_id=$2", eventID, userID)
-	if err != nil {
-		return err
-	}
-
-	rows, _ := res.RowsAffected()
-	if rows == 0 {
-		return errors.New("no booking found to cancel")
-	}
-
-	_, err = tx.ExecContext(ctx, "UPDATE events SET available_tickets = available_tickets + 1 WHERE id=$1", eventID)
-	if err != nil {
-		return err
-	}
-
-	return tx.Commit()
+	wg.wait()
 }
